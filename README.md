4d42559b3334fba482bc0ef85f7cca154ec5e897
Peng-Chi Fang <stephenfpc@u.northwestern.edu>
2023-01-17 12:26:02 -0600

    feat: stater code

---
+++ README.md
@@ -0,0 +1,72 @@
+# How to start
+1. Install Python3.7 via offical package ([link](https://www.python.org/downloads/)) or run the following commands on Unix systems.  
+```sudo apt-get install python3.7```
+
+2. Follow the **Project Setup** in the [doc](https://fetch-hiring.s3.amazonaws.com/data-engineer/pii-masking.pdf) to set up required Docker containers
+
+3. Instal required Python packages  
+```pip3 install -r requirements.txt```
+
+4. Run the application  
+```python3 starter.py```  
+<br/>
+
+# Development decisions
+
+## 1. How will you read messages from the queue?  
+- The program reads the messages from the SQS queue at most 10 messages in a batch, which is a given limitation for Boto3 package.  
+- For testing purpose, the program stops when there is no more messages for 30 seconds.  
+- In production env, the program can run continuously in the infinite while loop unless an incident happens.  
+<br/>
+
+## 2. What type of data structures should be used?  
+- After reading the messages from the SQS queue, a list of dictionaries is used to store the messages since the messages are in JSON format.  
+<br/>
+
+## 3. How will you mask the PII data so that duplicate values can be identified?  
+- The PII data is masked with AES - a symmetric encryption.  
+- The duplicated values will still be duplicates after encryption.  
+<br/>
+
+## 4. What will be your strategy for connecting and writing to Postgres?  
+- The connection to Postgres starts at the beginning of the program, and only closes before the program exits.  
+- The program writes the data to Postgres batch by batch based on the messages received.  
+<br/>
+
+## 5. Where and how will your application run?  
+- Assume the application can be run on a cloud service with the permissions to connect to other required containers.  
+<br/>
+
+---
+<br/>
+
+# Discussion questions  
+
+## 1. How would you deploy this application in production?  
+- The application can be deployed to a cloud container service, such as AWS ECS. 
+- Since the connected components are all internal services, the network setting should be set within a private subnet to ensure security.  
+<br/>  
+  
+## 2. What other components would you want to add to make this production ready?  
+- The application should respond 'ACK' to SQS queue to ensure no duplicated messages are consumed, and the erroneous messages that cannot be processed should be writted into logs.  
+- Data quality check should be added right after reading the messages.  
+- Unit tests and CI/CD can be added into deployment process for more efficient and error-free development cycle.  
+- Monitoring and logging are required for debugging and incident reports.  
+- For security concerns, the sensitive information, such as password, it should be stored safely. The AWS secrets manageer would be a good option.  
+<br/>
+    
+## 3. How can this application scale with a growing dataset?
+- Assume the incoming messages increase, need to make use of the auto-scale feature to create more containers to run the applcations.  
+- The database needs to support concurrent inserts for multiple applications to write into the database at the same time.  
+- If the dataset grows in a significant scale, which may be overwhelming for a relational databases, an alternative database might be considered depending on the usage of the data. For example, some NoSQL databases are more applicable for large datasets.  
+<br/>
+  
+## 4. How can PII be recovered later on?
+- PII is masked with AES, a symmetric encryption algorithms. It is feasible To recover the original values if the private key is given.  
+<br/>
+  
+## 5. What are the assumptions you made?
+- The messages are error-free and no data quality check is required.  
+- No duplicated data is produced and write all the data into the database.  
+- The messages are produced at a rate that an application is able to process. 
+- All the sensitve information shouldn't be shared in public, such as database.ini.
---
+++ main.py
@@ -0,0 +1,160 @@
+import boto3
+import json
+from botocore import exceptions
+import logging
+from psycopg2 import connect, sql, extras, DatabaseError
+from cryptography.fernet import Fernet
+from os import path
+from config import config
+
+
+class SQS:
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def connect(self):
+        self.sqs_client = boto3.client("sqs", endpoint_url="http://localhost:4566", region_name="", aws_access_key_id="", aws_secret_access_key="")
+
+    def receive_messages(self, query_url):
+        """
+        Retrieve the messages from SQS
+        """
+        try:
+            messages = self.sqs_client.receive_message(
+                QueueUrl=query_url,
+                MaxNumberOfMessages=10,
+                WaitTimeSeconds=30,
+            )
+
+            user_data = []
+            for message in messages.get("Messages", []):
+                message_body = json.loads(message["Body"])
+                user_data.append(message_body)
+        except exceptions.ClientError as error:
+            self.logger.exception("Couldn't receive messages from queue: %s", messages)
+            raise error
+        else:
+            return user_data
+
+class Mask():
+
+    def gen_key(self):
+        """
+        Generates a key and save it into a file
+        """
+        key = Fernet.generate_key()
+        with open("mask.key", "wb") as key_file:
+            key_file.write(key)
+        
+    def load_key(self):
+        """
+        Loads the key from the current directory named `mask.key`
+        """
+        return open("mask.key", "rb").read()
+
+    def mask_msg(self, user_data):
+        """
+        Enctypt the IP and device_id values with AEC.
+        Parse the integer value of the app_version.
+        """
+
+        key = self.load_key()
+        f = Fernet(key)
+        for data in user_data:
+            if 'ip' in data.keys():
+                data['masked_ip'] = f.encrypt(bytes(data.get('ip', ''), 'utf-8')).decode()
+            if 'device_id' in data.keys():
+                data['masked_device_id'] = f.encrypt(bytes(data.get('device_id', ''), 'utf-8')).decode()
+            if 'app_version' in data.keys():
+                data['app_version'] = data['app_version'].split('.')[0]
+        return user_data
+
+
+class Postgres():
+    
+    def connect(self):
+        """
+        Connect to Postgres SQL database
+        """
+
+        conn = None
+        try:
+            # read PostgreSQL connection parameters
+            params = config()
+
+            # connect to PostgreSQL server
+            self.psql_conn = connect(**params)
+            self.psql_cursor = self.psql_conn.cursor()
+        except (Exception, DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+                print('Database connection closed.')
+
+    def write(self, user_data, table):
+        """
+        Write data into PSQL tables
+        """
+
+        col_names = ['user_id', 'app_version', 'device_type', 'masked_ip', 'masked_device_id', 'locale']
+
+        values = []
+        for data in user_data:
+            value = []
+            for col in col_names:
+                if col not in data or data[col] is None:
+                    value.append(None)
+                else:
+                    value.append(str(data[col]))
+            values.append(value)
+
+        fields = ','.join(self.psql_cursor.mogrify("(%s,%s,%s,%s,%s,%s)", x).decode('utf-8') for x in values)
+
+        query = "INSERT INTO {table} ({col_names}) VALUES %s".format(table=table, col_names=','.join(col_names))
+        extras.execute_values(self.psql_cursor, query, values)
+
+        # query = sql.SQL("INSERT INTO {table} ({col_names}) VALUES ({fields});").format(
+        #     table=sql.Identifier(table),
+        #     col_names=sql.SQL(', ').join(map(sql.Identifier, col_names)),
+        #     fields=sql.Identifier(fields)
+        # )
+
+        self.psql_conn.commit()
+
+    def close(self):
+        """
+        Close Postgres SQL connection
+        """
+        self.psql_cursor.close()
+        self.psql_conn.close()
+
+if __name__ == "__main__":
+
+    sqs = SQS()
+    sqs.connect()
+    mask = Mask()
+    postgres = Postgres()
+    postgres.connect()
+
+    if (not path.exists("mask.key")):
+        mask.gen_key()
+
+    # see if there is a blocking function to wait for the messages
+    messages = sqs.receive_messages("/000000000000/login-queue")
+    while(len(messages)):
+        data = mask.mask_msg(messages)
+
+        # Should check data integrity before writing
+        if (len(data)):
+            postgres.write(data, "user_logins")
+
+        postgres_cur = postgres.psql_conn.cursor()
+        postgres_cur.execute(sql.SQL(f"SELECT count(*), count(distinct(user_id)), count(distinct(masked_ip)), count(distinct(masked_device_id)) FROM user_logins LIMIT 100;"))
+        res = postgres_cur.fetchall()
+        print(res)
+
+        messages = sqs.receive_messages("/000000000000/login-queue")
+    
+    postgres.close()
